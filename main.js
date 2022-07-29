3501367a5b038cc5b3f119c782c51b7ea023c1fa
Jamie Furlong <jamiefurlong16@gmail.com>
2022-07-29 17:03:20 +0100

    Multiple Round Simulation
    Added in game() function.
    This function allows 5-round game simulations.
    
    Other changes include:
    - Input validation in both gametypes.
    - New initial input to prompt user for the type of game.

--- main.js
+++ main.js
@@ -1,16 +1,37 @@
 let arr = ['Rock', 'Paper', 'Scissors'];
+let gameType;
 
-// Get user input and format the string so that only the first letter is capitalised.
-let playerChoice = prompt("Please choose rock, paper or scissors: ");
-let firstLetter = playerChoice.charAt(0).toUpperCase();
-playerChoice = playerChoice.slice(1).toLowerCase();
-playerChoice = firstLetter + playerChoice;
-console.log(`Player choice = ${playerChoice}`);
+// Decide if the player wants a single or 5-round game.
+do {
+    gameType = prompt("Please enter single or multiple for the type of game you wish to play.");
+    gameType = gameType.toLowerCase();
+    console.log(gameType);
+}
+while (gameType !== "single" && gameType!=="multiple")
 
-// Get computer's choice.
-let computerSelection = getComputerChoice();
-console.log(`Computer Choice: ${computerSelection}`);
-console.log(playSingleRound(playerChoice, computerSelection));
+if (gameType === "single") {
+    // Get user input and format the string so that only the first letter is capitalised.
+    let playerChoice;
+    let firstLetter;
+    do {
+        playerChoice = prompt("Please choose rock, paper or scissors: ");
+        firstLetter = playerChoice.charAt(0).toUpperCase();
+        playerChoice = playerChoice.slice(1).toLowerCase();
+        playerChoice = firstLetter + playerChoice;
+    }
+    while (playerChoice !== "Rock" && playerChoice!=="Paper" && playerChoice!=="Scissors")
+    
+    console.log(`Player choice = ${playerChoice}`);
+
+    // Get computer's choice.
+    let computerSelection = getComputerChoice();
+    console.log(`Computer Choice: ${computerSelection}`);
+    console.log(playSingleRound(playerChoice, computerSelection));
+} else {
+    console.log(game());
+}
+
+
 
 // Computer will generate a random number between 0 and 2 and then returned it corresponding value in the array.
 function getComputerChoice () {
@@ -37,4 +58,39 @@ function playSingleRound (playerChoice, computerSelection) {
         return `You draw. ${computerSelection} can't beat ${playerChoice}.`;
     }
 
+}
+
+// Simulate multiple rounds.
+function game() {
+    let computer = 0;
+    let player = 0;
+    let playerChoice;
+    let computerSelection;
+    let firstLetter;
+    let winner;
+
+    for (let i = 0; i < 5; i++) {
+
+        do {
+            playerChoice = prompt("Please choose rock, paper or scissors: ");
+            firstLetter = playerChoice.charAt(0).toUpperCase();
+            playerChoice = playerChoice.slice(1).toLowerCase();
+            playerChoice = firstLetter + playerChoice;
+        }
+        while (playerChoice !== "Rock" && playerChoice!=="Paper" && playerChoice!=="Scissors")
+
+        computerSelection = getComputerChoice()
+
+        winner = playSingleRound(playerChoice, computerSelection);
+        winner = winner.charAt(4);
+        if (winner === "w") player++; else if (winner === "l") computer++;
+    }
+
+    if (computer > player) {
+        return `You lose. Computer wins ${computer} - ${player}.`;
+    } else if (player > computer) {
+        return `You win. Player wins ${player} - ${computer}.`;
+    } else {
+        return `You draw ${computer} - ${player}.`;
+    }
 }
